c0de68970ee0fd8d8c9595b6f57ea824b7ab28f9
yukno1 <121278340+yukno1@users.noreply.github.com>
2025-09-29 22:19:08 +0800

    include opengl32.lib; it's included in system path

--- Miriya/src/Miriya/Application.cpp
+++ Miriya/src/Miriya/Application.cpp
@@ -17,8 +17,8 @@ namespace Miriya {
 
     void Application::Run() {
         while (m_Running) {
-            // glClearColor(1, 0, 1, 1);
-            // glClear(GL_COLOR_BUFFER_BIT);
+            glClearColor(1, 0, 1, 1);
+            glClear(GL_COLOR_BUFFER_BIT);
             m_Window->OnUpdate();
         }
 
